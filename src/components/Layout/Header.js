97211551868528425ac9eec8e0166ce088a57bd9
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2023-05-04 09:55:35 +0430

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from "react";
+import classes from './Header.module.css'
+import Pizza from '../../assets/pizza.jpg'
+
+const Header = () => {
+    return(
+    <Fragment>
+        <header className={classes.header}>
+            <h1>Fancy Restaurant</h1>
+            <button className={classes.button}>
+                Cart
+            </button>
+        </header>
+        <div className={classes["our-image"]}>
+            <img src={Pizza} alt='Delicious Pizza' />
+        </div>
+    </Fragment>
+    )
+}
+
+export default Header;
