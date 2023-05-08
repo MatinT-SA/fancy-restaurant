b0abb0bd55ad06621136a04c67475e10f2148236
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2023-05-08 15:32:31 +0430

    adding badge and icon to cart button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,14 @@
 import React, { Fragment } from "react";
 import classes from './Header.module.css'
 import Pizza from '../../assets/pizza.jpg'
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = () => {
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>Fancy Restaurant</h1>
-                <button className={classes.button}>
-                    <span className={classes["button-front"]}>
-                        Cart
-                    </span>
-                </button>
+                <HeaderCartButton />
             </header>
             <div className={classes["our-image"]}>
                 <img src={Pizza} alt='Delicious Pizza' />
