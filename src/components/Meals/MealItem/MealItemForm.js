75ac9777be6ed4a2bc680808b828f57af5318c35
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-04-25 21:56:54 +0330

    submitHandler MealItemForm

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,16 +1,33 @@
+import { useRef } from 'react';
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css'
 
 const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amount" input={{
-            id: 'amount' + props.id,
-            type: 'number',
-            min: '1',
-            max: '5',
-            step: '1',
-            defaultValue: '1'
-        }} />
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enteredAmountNumber = +enteredAmount;
+
+        if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5){
+            return;
+        }
+    }
+
+    return <form className={classes.form} onSubmit={submitHandler}>
+        <Input
+            ref={amountInputRef}
+            label="Amount"
+            input={{
+                id: 'amount' + props.id,
+                type: 'number',
+                min: '1',
+                max: '5',
+                step: '1',
+                defaultValue: '1'
+            }} />
         <button>+ Add</button>
     </form>
 }
