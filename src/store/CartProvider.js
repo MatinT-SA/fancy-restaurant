05f91fa13c3c8a2282a2fcb9d12ef8109f4aa7a0
MatinT-SA <matin.taherzadeh.mmtsa@gmail.com>
2024-05-04 22:43:59 +0330

    Checking the existence of items in our cart

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,15 +1,30 @@
 import { useReducer } from "react";
 import CartContext from "./cart-context";
 
-const defaultCartState ={
+const defaultCartState = {
     items: [],
     totalAmount: 0
 }
 
-const cartReducer = (state, action) =>{
-    if(action.type === 'ADD_ITEM'){
-        const updatedItems = state.items.concat(action.item);
+const cartReducer = (state, action) => {
+    if (action.type === 'ADD_ITEM') {
         const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+
+        const existingCartItemIndex = state.items.findIndex(item => item.id === action.item.id);
+        const existingCartItem = state.items[existingCartItemIndex];
+        let updatedItems;
+        
+        if(existingCartItem){
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }else{
+            updatedItems = state.items.concat(action.item);
+        }
+
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
@@ -19,14 +34,14 @@ const cartReducer = (state, action) =>{
 };
 
 const CartProvider = props => {
-    const [cartState, dispatchCartAction] = useReducer(cartReducer,defaultCartState);
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const addItemCartHandler = item =>{
-        dispatchCartAction({type: 'ADD_ITEM', item: item});
+    const addItemCartHandler = item => {
+        dispatchCartAction({ type: 'ADD_ITEM', item: item });
     }
 
-    const removeItemCartHandler = id =>{
-        dispatchCartAction({type: 'REMOVE_ITEM', id: id});
+    const removeItemCartHandler = id => {
+        dispatchCartAction({ type: 'REMOVE_ITEM', id: id });
     }
 
     const cartContext = {
